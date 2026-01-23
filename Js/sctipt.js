b6dd7ed6d60343c0737e8dccdc99e2791bae5dc3
Shipon Roy <shiponworks@gmail.com>
2026-01-23 16:37:42 +0600

    Completes: Hero section and Navitation Manu

--- Js/sctipt.js
+++ Js/sctipt.js
@@ -1,1 +1,13 @@
 const ul = document.getElementById('Navigation_menu');
+const mobileBars = document.getElementById('MobileBars');
+
+const openIcon = document.getElementById('openIcon');
+const closeIcon = document.getElementById('closeIcon');
+
+mobileBars.addEventListener('click', () => {
+  ul.classList.toggle('-translate-x-full');
+  ul.classList.toggle('-translate-x-0');
+
+  openIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
+});
